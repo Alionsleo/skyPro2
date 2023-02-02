6b571384684bbb3a652d8a46835e793709bf86ee
Alena Ilina <aljona.iljina@njsoft.ru>
2023-02-02 23:31:13 +0300

    правки

--- script.js
+++ script.js
@@ -42,13 +42,7 @@ for(let i = 0; i < mass1.length; i++) {
 
 //Задание 2
 const mass2 = [1, 5, 4, 10, 0, 3];
-let key = 0;
-for(let i = 0; i < mass2.length; i++) {
-    if(mass2[i] == 4) {
-        key = i;
-        break;
-    }
-}
+console.log(mass2.indexOf(4));
 
 console.log(`Позиция 4 в массиве ${mass2} - ${key}`);
 
@@ -75,7 +69,7 @@ for(let i = 0; i < 3; i++) {
 console.log(mass);
 //Задание 6
 let mass6 = [1, 1, 1];
-console.log(mass6.concat([2, 2, 2]));
+console.log(mass6.push(2, 2, 2));
 
 //Задание 7
 let mass7 = [9, 8, 7, 'a', 6, 5];
@@ -85,28 +79,23 @@ console.log(mass7);
 
 //Задание 8 //демо forEach, но можно проще
 const mass8 = [9, 8, 7, 6, 5];
-let check = +prompt(`Введите число от 0 до 10`);
+let check = +prompt(`Введите число от 1 до 10`);
 let res = false;
 if(!isNaN(check) && check >= 1 && check <= 10) {
-    mass8.forEach(item => {
-        if(item === check) res = true;
-    });
+    res = mass8.includes(check);
 }
 if(res) console.log(`Число ${check} содержится в массиве [${mass8.join(', ')}]`);
 else console.log(`Число ${check} не содержится в массиве [${mass8.join(', ')}]`)
 //Задание 9
-let str = 'abcdef'.split('');
-let strReplace = [];
-const strLen = str.length;
-for(let i = 0; i < strLen; i++) 
-    strReplace.push(str.pop());
-console.log(strReplace.join(''));
+let str = 'abcdef'.split('').reverse().join('');
+
+console.log(str);
 
 //Задание 10
 let mass10 = [];
 let sum = 0;
 for(let i = 0; i < 6; i++) {
-    mass10.push(Math.floor(Math.random() * 10));
+    mass10.push(Math.floor(1 + Math.random() * 10));
     sum += mass10[i];
 }
 sum /= mass10.length;
@@ -114,13 +103,13 @@ console.log(`Среднее арифметическое чисел [${mass10.jo
 
 //Задание 11
 let mass11 = [[1, 2, 3,],[4, 5, 6]];
-console.log(`[${mass11[0].concat(mass11[1]).join(', ')}]`);
+console.log(`[${mass11.flat().join(', ')}]`);
 
 //Задание 12 //сделала обход одним циклом
 let mass12 = [];
 const count = 6;
 for(let i = 0; i < count; i++) {
-    mass12.push(Math.floor(Math.random() * 10));
+    mass12.push(1 + Math.floor(Math.random() * 10));
     if(i > 0 && i <= count - 1) console.log(`Сумма ${i-1}-го и ${i}-го элементов равна ${mass12[i-1] + mass12[i]}`);
 }
 console.log(`Начальный массив [${mass12.join(', ')}]`);
